f08fd685fcc1975077f4da0a53934d3afaa29526
giakiet05 <giakiet05@gmail.com>
2025-05-21 20:07:37 +0700

    rename package and change id to string

---
+++ backend/server/model/order.go
@@ -0,0 +1,34 @@
+package model
+
+import (
+	"time"
+)
+
+type ShopOrder struct {
+	ID              string `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	UserId          string `gorm:"type:uuid"`
+	PostId          string `gorm:"type:uuid"`
+	PaymentMethodId string `gorm:"type:uuid"`
+	Status          string
+	AddressId       string `gorm:"type:uuid"`
+	Total           int
+	CreatedAt       time.Time
+	CompletedAt     time.Time
+	CanceledAt      time.Time
+
+	User          *User
+	Post          *Post
+	PaymentMethod *PaymentMethod
+	Address       *Address
+}
+
+type UserReview struct {
+	UserId    string `gorm:"type:uuid;primaryKey"`
+	OrderId   string `gorm:"type:uuid;primaryKey"`
+	Rating    int
+	Comment   string
+	CreatedAt time.Time
+
+	User  *User
+	Order *ShopOrder
+}
