5a8fbd195e61643d5561beec7c023ef68e075352
giakiet05 <giakiet05@gmail.com>
2025-05-23 16:02:04 +0700

    modify db

--- backend/server/transaction/category_request.go
+++ backend/server/transaction/category_request.go
@@ -1,7 +1,8 @@
 package transaction
 
 type CreateCategoryRequest struct {
-	Name string `json:"name" binding:"required"`
+	ParentCategoryId *string `json:"parent_category_id"`
+	Name             string  `json:"name" binding:"required"`
 }
 
 type UpdateCategoryRequest struct {
