4e2cdec9c3e71871bd786675a1cf9ae02180aa6a
giakiet05 <giakiet05@gmail.com>
2025-05-24 08:07:53 +0700

    hello

--- backend/server/model/cart.go
+++ backend/server/model/cart.go
@@ -1,16 +1,16 @@
 package model
 
 type Cart struct {
-	ID     string  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	UserID *string `gorm:"type:uuid"`
+	ID     string  `gorm:"type:uuid;primary_key" json:"id"`
+	UserID *string `gorm:"type:uuid" json:"user_id"`
 
-	User *User
+	User *User `json:"user,omitempty"`
 }
 
 type CartItem struct {
-	CartID *string `gorm:"type:uuid;primaryKey"`
-	PostID *string `gorm:"type:uuid;primaryKey"`
+	CartID *string `gorm:"type:uuid;primaryKey" json:"cart_id"`
+	PostID *string `gorm:"type:uuid;primaryKey" json:"post_id"`
 
-	Cart *Cart
-	Post *Post
+	Cart *Cart `json:"cart,omitempty"`
+	Post *Post `json:"post,omitempty"`
 }
