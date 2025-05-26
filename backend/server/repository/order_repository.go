6524a6679e14fedcde527076cadbbf0ea0fff791
Võ An Khôi <voankhoi@gmail.com>
2025-05-26 19:56:41 +0700

    Finish order repo and fix some small bug

--- backend/server/repository/order_repository.go
+++ backend/server/repository/order_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"github.com/VoAnKhoi2005/ReSell/model"
+	"github.com/VoAnKhoi2005/ReSell/util"
 	"gorm.io/gorm"
 )
 
@@ -10,6 +11,14 @@ type OrderRepository interface {
 	Create(order *model.ShopOrder) error
 	Update(order *model.ShopOrder) error
 	Delete(order *model.ShopOrder) error
+
+	GetByID(orderID string) (*model.ShopOrder, error)
+	GetByPostID(PostID string) (*model.ShopOrder, error)
+	GetByBuyerID(BuyerID string) (*model.ShopOrder, error)
+
+	CreateReview(review *model.UserReview) error
+	GetReviewByID(reviewID string) (*model.UserReview, error)
+	DeleteReview(review *model.UserReview) error
 }
 
 type oderRepository struct {
@@ -18,4 +27,54 @@ type oderRepository struct {
 
 func NewOrderRepository(db *gorm.DB) OrderRepository {
 	return &oderRepository{BaseRepository: NewBaseRepository[model.ShopOrder](db)}
+}
+
+func (o *oderRepository) GetByID(orderID string) (*model.ShopOrder, error) {
+	ctx, cancel := util.NewDBContext()
+	defer cancel()
+
+	var order *model.ShopOrder = nil
+	err := o.db.WithContext(ctx).First(&order, orderID).Error
+	return order, err
+}
+
+func (o *oderRepository) GetByPostID(PostID string) (*model.ShopOrder, error) {
+	ctx, cancel := util.NewDBContext()
+	defer cancel()
+
+	var order *model.ShopOrder = nil
+	err := o.db.WithContext(ctx).First(&order, "post_id = ?", PostID).Error
+	return order, err
+}
+
+func (o *oderRepository) GetByBuyerID(BuyerID string) (*model.ShopOrder, error) {
+	ctx, cancel := util.NewDBContext()
+	defer cancel()
+
+	var order *model.ShopOrder = nil
+	err := o.db.WithContext(ctx).First(&order, "user_id = ?", BuyerID).Error
+	return order, err
+}
+
+func (o *oderRepository) CreateReview(review *model.UserReview) error {
+	ctx, cancel := util.NewDBContext()
+	defer cancel()
+
+	return o.db.WithContext(ctx).Create(review).Error
+}
+
+func (o *oderRepository) GetReviewByID(reviewID string) (*model.UserReview, error) {
+	ctx, cancel := util.NewDBContext()
+	defer cancel()
+
+	var review *model.UserReview = nil
+	err := o.db.WithContext(ctx).First(&review, reviewID).Error
+	return review, err
+}
+
+func (o *oderRepository) DeleteReview(review *model.UserReview) error {
+	ctx, cancel := util.NewDBContext()
+	defer cancel()
+
+	return o.db.WithContext(ctx).Delete(review).Error
 }
--- backend/server/service/order_service.go
+++ backend/server/service/order_service.go
@@ -1,14 +1,76 @@
 package service
 
-import "github.com/VoAnKhoi2005/ReSell/repository"
+import (
+	"github.com/VoAnKhoi2005/ReSell/model"
+	"github.com/VoAnKhoi2005/ReSell/repository"
+)
 
 type OrderService interface {
+	CreateOrder(order *model.ShopOrder) error
+	DeleteOrder(orderID string) error
+
+	GetByID(orderID string) (*model.ShopOrder, error)
+	GetByPostID(PostID string) (*model.ShopOrder, error)
+	GetByBuyerID(BuyerID string) (*model.ShopOrder, error)
+
+	UpdateStatus(orderID string, status string) error
+
+	CreateReview(review *model.UserReview) error
+	DeleteReview(reviewID string) error
 }
 
 type OderService struct {
-	orderService *repository.OrderRepository
+	orderRepository repository.OrderRepository
 }
 
-func NewOrderService(repo *repository.OrderRepository) OrderService {
-	return &OderService{orderService: repo}
+func NewOrderService(repo repository.OrderRepository) OrderService {
+	return &OderService{orderRepository: repo}
+}
+
+func (o *OderService) CreateOrder(order *model.ShopOrder) error {
+	return o.orderRepository.Create(order)
+}
+
+func (o *OderService) DeleteOrder(orderID string) error {
+	order, err := o.orderRepository.GetByID(orderID)
+	if err != nil {
+		return err
+	}
+
+	return o.orderRepository.Delete(order)
+}
+
+func (o *OderService) GetByID(orderID string) (*model.ShopOrder, error) {
+	return o.orderRepository.GetByID(orderID)
+}
+
+func (o *OderService) GetByPostID(PostID string) (*model.ShopOrder, error) {
+	return o.orderRepository.GetByPostID(PostID)
+}
+
+func (o *OderService) GetByBuyerID(BuyerID string) (*model.ShopOrder, error) {
+	return o.orderRepository.GetByBuyerID(BuyerID)
+}
+
+func (o *OderService) UpdateStatus(orderID string, status string) error {
+	order, err := o.orderRepository.GetByID(orderID)
+	if err != nil {
+		return err
+	}
+
+	order.Status = status
+	return o.orderRepository.Update(order)
+}
+
+func (o *OderService) CreateReview(review *model.UserReview) error {
+	return o.orderRepository.CreateReview(review)
+}
+
+func (o *OderService) DeleteReview(reviewID string) error {
+	review, err := o.orderRepository.GetReviewByID(reviewID)
+	if err != nil {
+		return err
+	}
+	
+	return o.orderRepository.DeleteReview(review)
 }
