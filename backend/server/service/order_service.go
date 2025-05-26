1f399dfaede0effd298deb94dfdc07d830e385a0
Võ An Khôi <voankhoi@gmail.com>
2025-05-26 19:22:22 +0700

    Init

--- backend/server/service/order_service.go
+++ backend/server/service/order_service.go
@@ -1,1 +1,14 @@
 package service
+
+import "github.com/VoAnKhoi2005/ReSell/repository"
+
+type OrderService interface {
+}
+
+type OderService struct {
+	orderService *repository.OrderRepository
+}
+
+func NewOrderService(repo *repository.OrderRepository) OrderService {
+	return &OderService{orderService: repo}
+}
