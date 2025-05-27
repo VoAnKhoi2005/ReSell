fa222a2753f8cde0ffe65d581b20fb7514ba21c2
giakiet05 <giakiet05@gmail.com>
2025-05-27 15:22:14 +0700

    add tag to models

--- backend/server/model/payment.go
+++ backend/server/model/payment.go
@@ -1,34 +1,28 @@
-package model
-
-import (
-	"time"
-)
-
 type PaymentMethod struct {
-	ID   string `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Name string
+ID   string `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+Name string `json:"name"`
 }
 
 type Wallet struct {
-	ID            string  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	UserID        *string `gorm:"type:uuid"`
-	Balance       uint
-	FrozenBalance uint
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
+ID            string  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+UserID        *string `gorm:"type:uuid" json:"user_id"`
+Balance       uint    `json:"balance"`
+FrozenBalance uint    `json:"frozen_balance"`
+CreatedAt     time.Time `json:"created_at"`
+UpdatedAt     time.Time `json:"updated_at"`
 
-	User *User
+User *User `json:"user,omitempty"`
 }
 
 type WalletTransaction struct {
-	ID           string  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	WalletId     *string `gorm:"type:uuid"`
-	Type         string
-	Amount       int
-	BalanceAfter int
-	OrderId      *string `gorm:"type:uuid"`
-	CreatedAt    time.Time
+ID           string  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+WalletId     *string `gorm:"type:uuid" json:"wallet_id"`
+Type         string  `json:"type"`
+Amount       int     `json:"amount"`
+BalanceAfter int     `json:"balance_after"`
+OrderId      *string `gorm:"type:uuid" json:"order_id"`
+CreatedAt    time.Time `json:"created_at"`
 
-	Wallet *Wallet
-	Order  *ShopOrder
+Wallet *Wallet     `json:"wallet,omitempty"`
+Order  *ShopOrder  `json:"order,omitempty"`
 }
