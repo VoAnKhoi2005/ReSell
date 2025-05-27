45ee538ad4ee468016b97912737c853158f50a5c
giakiet05 <giakiet05@gmail.com>
2025-05-27 15:23:18 +0700

    fix smt

--- backend/server/model/message.go
+++ backend/server/model/message.go
@@ -1,22 +1,26 @@
-type Conversation struct {
-ID        string  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-BuyerId   *string `gorm:"type:uuid" json:"buyer_id"`
-SellerId  *string `gorm:"type:uuid" json:"seller_id"`
-PostId    *string `gorm:"type:uuid" json:"post_id"`
-CreatedAt time.Time `json:"created_at"`
+package model
 
-Buyer  *User `json:"buyer,omitempty"`
-Seller *User `json:"seller,omitempty"`
-Post   *Post `json:"post,omitempty"`
+import "time"
+
+type Conversation struct {
+	ID        string    `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	BuyerId   *string   `gorm:"type:uuid" json:"buyer_id"`
+	SellerId  *string   `gorm:"type:uuid" json:"seller_id"`
+	PostId    *string   `gorm:"type:uuid" json:"post_id"`
+	CreatedAt time.Time `json:"created_at"`
+
+	Buyer  *User `json:"buyer,omitempty"`
+	Seller *User `json:"seller,omitempty"`
+	Post   *Post `json:"post,omitempty"`
 }
 
 type Message struct {
-ID             string  `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-ConversationId *string `gorm:"type:uuid" json:"conversation_id"`
-SenderId       *string `gorm:"type:uuid" json:"sender_id"`
-Content        string  `json:"content"`
-CreatedAt      time.Time `json:"created_at"`
+	ID             string    `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	ConversationId *string   `gorm:"type:uuid" json:"conversation_id"`
+	SenderId       *string   `gorm:"type:uuid" json:"sender_id"`
+	Content        string    `json:"content"`
+	CreatedAt      time.Time `json:"created_at"`
 
-Conversation *Conversation `json:"conversation,omitempty"`
-Sender       *User         `json:"sender,omitempty"`
+	Conversation *Conversation `json:"conversation,omitempty"`
+	Sender       *User         `json:"sender,omitempty"`
 }
