901762e7ba36a460eb86b1b29b6cf43c6caabb71
giakiet05 <giakiet05@gmail.com>
2025-05-30 22:09:04 +0700

    handle cors in backend

--- backend/server/main.go
+++ backend/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/VoAnKhoi2005/ReSell/config"
 	"github.com/VoAnKhoi2005/ReSell/route"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
 	"os"
@@ -20,11 +21,22 @@ func main() {
 	config.ConnectDatabase()
 	config.RunMigrations()
 
-	router := gin.Default()
-	route.SetupRoutes(router)
+	r := gin.Default()
+	allowOrigin := os.Getenv("FRONTEND_URL")
+	if allowOrigin == "" {
+		allowOrigin = "http://localhost:5173"
+	}
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{allowOrigin},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
+	route.SetupRoutes(r)
 
 	log.Printf("Server is running at http://localhost:%s\n", port)
-	err := router.Run(":" + port)
+	err := r.Run(":" + port)
 	if err != nil {
 		log.Fatalln("Failed to start server: ", err)
 	}
