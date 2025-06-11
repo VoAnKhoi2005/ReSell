f56c0e4951aea4cb753d3a9a61bd28baddb166c2
Thanh456 <pmtcyty@gmail.com>
2025-06-11 22:49:35 +0700

    init_navigation

---
+++ frontend/ReSell/app/src/main/java/com/example/myapplication/ui/navigation/NavGraph.kt
@@ -0,0 +1,29 @@
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.myapplication.ui.navigation.Screen
+import com.example.myapplication.ui.screen.login.LoginScreen
+import com.example.myapplication.ui.screen.register.RegisterScreen
+
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController
+){
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Register.route
+    ){
+        composable(
+            route = Screen.Login.route
+        ) {
+            LoginScreen()
+        }
+        composable(
+            route = Screen.Register.route
+        ) {
+            RegisterScreen()
+        }
+    }
+}
---
+++ frontend/ReSell/app/src/main/java/com/example/myapplication/ui/navigation/Screen.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.navigation
+
+sealed class Screen(val route: String){
+    object Login: Screen(route = "login_screen")
+    object Register: Screen(route = "register_screen")
+
+}
