a5539a3ce06be855a8b578370768eefb6e7618de
Thanh456 <pmtcyty@gmail.com>
2025-06-12 03:56:23 +0700

    Init_NavigateForBottombar_ListViewOnPost

---
+++ frontend/ReSell/app/src/main/java/com/example/myapplication/ui/screen/newsmanagement/PostManagementView.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.ui.screen.newsmanagement
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PostMangamentScreen()
+{
+    Text("This is ManagamentScreen")
+}
