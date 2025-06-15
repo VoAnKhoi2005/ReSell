d46dadd342fd61b3498598abd38de45dba30d9b8
Võ An Khôi <voankhoi@gmail.com>
2025-06-15 22:01:14 +0700

    Encrypt jwt token FE

--- frontend/ReSell/app/src/main/java/store/TokenManager.kt
+++ frontend/ReSell/app/src/main/java/store/TokenManager.kt
@@ -1,6 +1,7 @@
 package store
 
 import android.content.Context
+import android.content.SharedPreferences
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -8,23 +9,23 @@ import androidx.core.content.edit
 import model.AuthToken
 
 @Singleton
-class TokenManager @Inject constructor(@ApplicationContext private val context: Context){
-    private val prefs = context.getSharedPreferences("auth_prefs", Context.MODE_PRIVATE)
-
+class TokenManager @Inject constructor(
+    private val sharedPrefs: SharedPreferences
+){
     fun saveToken(token: AuthToken) {
-        prefs.edit { putString("jwt_access_token", token.accessToken) }
-        prefs.edit { putString("jwt_refresh_token", token.refreshToken) }
+        sharedPrefs.edit { putString("jwt_access_token", token.accessToken) }
+        sharedPrefs.edit { putString("jwt_refresh_token", token.refreshToken) }
     }
 
     fun getAccessToken(): String? {
-        return prefs.getString("jwt_access_token", null)
+        return sharedPrefs.getString("jwt_access_token", null)
     }
 
     fun getRefreshToken(): String? {
-        return prefs.getString("jwt_refresh_token", null)
+        return sharedPrefs.getString("jwt_refresh_token", null)
     }
 
     fun clearToken() {
-        prefs.edit { clear() }
+        sharedPrefs.edit { clear() }
     }
 }
