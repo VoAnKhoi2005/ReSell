2a418d4ffcb320dffbb4f8660c8eb6ba82da80b9
Thanh456 <pmtcyty@gmail.com>
2025-06-15 10:20:23 +0700

    added_profilescreenUI

--- frontend/ReSell/app/src/main/java/com/example/resell/ui/components/NavigationItems.kt
+++ frontend/ReSell/app/src/main/java/com/example/resell/ui/components/NavigationItems.kt
@@ -1,24 +1,34 @@
 package com.example.resell.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.resell.ui.theme.GrayFont
+import com.example.resell.ui.theme.LightGray
 
 @Composable
 fun IconButtonVertical(//nút dưới bottombar
@@ -48,5 +58,34 @@ fun IconButtonVertical(//nút dưới bottombar
             text = label,
             style = MaterialTheme.typography.labelMedium.copy(fontWeight= FontWeight.Light, fontSize = fontSize)
         )
+    }
+}
+@Composable
+fun IconButtonHorizontal(
+    text: String,
+    iconResId: Int,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(4.dp),
+        border = BorderStroke(1.dp, LightGray),
+        contentPadding = PaddingValues(horizontal = 2.dp, vertical = 8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                painter = painterResource(id = iconResId),
+                contentDescription = null,
+                modifier = Modifier.size(26.dp),
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.run { width(4.dp) })
+            Text(text, color = GrayFont, style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold, fontSize = 16.sp))
+        }
     }
 }
